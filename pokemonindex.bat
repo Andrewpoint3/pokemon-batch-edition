0b8ec8d870ba4d20d3405837dc4b34f216959511
Andrewpoint3 <50642269+Andrewpoint3@users.noreply.github.com>
2019-06-13 19:27:42 -0500

    Pokemon index 1-9
    All gen 1 starters and their evolutions added in

---
+++ pokemonindex.bat
@@ -0,0 +1,139 @@
+set /a p1=Bulbasaur
+set /a p1atk=5
+set /a p1def=5
+set /a p1spd=5
+set /a p1hp=20
+set p1t1=Grass
+set p1t2=Poison
+set p1m1=tackle
+set p1m2=splash
+set p1m3=splash
+set p1m4=splash
+
+set /a p2=Ivysaur
+set /a p2atk=10
+set /a p2def=10
+set /a p2spd=10
+set /a p2hp=40
+set p2t1=Grass
+set p2t2=Poison
+set p2m1=tackle
+set p2m2=splash
+set p2m3=splash
+set p2m4=splash
+
+set /a p3=Venusaur
+set /a p3atk=10
+set /a p3def=10
+set /a p3spd=10
+set /a p3hp=40
+set p3t1=Grass
+set p3t1=Poison
+set p3m1=tackle
+set p3m2=splash
+set p3m3=splash
+set p3m4=splash
+
+set /a p4=charmander
+set /a p4atk=10
+set /a p4def=10
+set /a p4spd=10
+set /a p4hp=40
+set p4t1=Fire
+set p4t1=None
+set p4m1=tackle
+set p4m2=splash
+set p4m3=splash
+set p4m4=splash
+
+set /a p5=charmeleon
+set /a p5atk=10
+set /a p5def=10
+set /a p5spd=10
+set /a p5hp=40
+set p5t1=Fire
+set p5t1=None
+set p5m1=tackle
+set p5m2=splash
+set p5m3=splash
+set p5m4=splash
+
+set /a p6=Charizard
+set /a p6atk=10
+set /a p6def=10
+set /a p6spd=10
+set /a p6hp=40
+set p6t1=Grass
+set p6t1=Poison
+set p6m1=tackle
+set p6m2=splash
+set p6m3=splash
+set p6m4=splash
+
+set /a p7=Squirtle
+set /a p7atk=10
+set /a p7def=10
+set /a p7spd=10
+set /a p7hp=40
+set p7t1=Grass
+set p7t1=Poison
+set p7m1=tackle
+set p7m2=splash
+set p7m3=splash
+set p7m4=splash
+
+set /a p8=Wartortle
+set /a p8atk=10
+set /a p8def=10
+set /a p8spd=10
+set /a p8hp=40
+set p8t1=Grass
+set p8t1=Poison
+set p8m1=tackle
+set p8m2=splash
+set p8m3=splash
+set p8m4=splash
+
+set /a p9=Blastoise
+set /a p9atk=10
+set /a p9def=10
+set /a p9spd=10
+set /a p9hp=40
+set p9t1=Grass
+set p9t1=Poison
+set p9m1=tackle
+set p9m2=splash
+set p9m3=splash
+set p9m4=splash
+
+cmd /k
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
